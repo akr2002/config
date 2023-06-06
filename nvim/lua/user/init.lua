9f5b642cc498a14051c27c7c2c1ed724139070cf
aditya <bluenerd@protonmail.com>
2023-06-06 18:08:49 +0530

    modify nvim theme

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -26,7 +26,7 @@ local config = {
   },
 
   -- Set colorscheme to use
-  colorscheme = "zellner",
+  --colorscheme = "default", --zellner",
 
   -- Add highlight groups in any theme
   highlights = {
